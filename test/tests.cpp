cd87cc4e4160340353f1ab84987b74c6e0338ca4
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-02 18:51:26 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -125,3 +125,11 @@ TEST(SumPrimeTest, LargerNumbers) {
 
 TEST(st1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
+  uint64_t expected = 142913828922;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, sumPrime2) {
+  uint64_t res = sumPrime(10);
+  uint64_t expected = 17;
+  EXPECT_EQ(expected, res);
+}
