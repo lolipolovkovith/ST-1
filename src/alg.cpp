c2b74bc1c01317b357b39f96a2ca7fd81d1c3ddc
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-02 18:44:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,75 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
+#include <vector>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+  if (value <= 1) {
+    return false;
+  }
+
+  bool isPrime = true;
+
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if (value % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  return isPrime;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) return 0;
+
+  uint64_t count = 0;
+  uint64_t num = 2;
+
+  while (count < n) {
+    if (checkPrime(num)) {
+      count++;
+      if (count == n) {
+        return num;
+      }
+    }
+    num++;
+  }
+
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t num = value + 1;
+
+  while (!checkPrime(num)) {
+    num++;
+  }
+
+  return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  if (hbound <= 2) return 0;
+
+  std::vector<bool> sieve(hbound, true);
+  sieve[0] = sieve[1] = false;
+
+  for (uint64_t i = 2; i * i < hbound; i++) {
+    if (sieve[i]) {
+      for (uint64_t j = i * i; j < hbound; j += i) {
+        sieve[j] = false;
+      }
+    }
+  }
+
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (sieve[i]) {
+      sum += i;
+    }
+  }
+
+  return sum;
 }
